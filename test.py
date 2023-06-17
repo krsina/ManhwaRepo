61abce937c5038cbb9e027100cef3bbe52757f1b
krsina <krisnaoeurn@gmail.com>
2023-06-16 23:47:33 -0700

    scrapes multiple books now

--- test.py
+++ test.py
@@ -9,27 +9,36 @@ chrome_options = Options()
 chrome_options.add_argument("--disable-gpu")  # Disable GPU acceleration
 chrome_options.add_argument("--headless=new")
 
+# Read the book links from the books.txt file
+with open("books.txt", "r") as file:
+    book_links = file.read().splitlines()
+
 # Set up the driver with the Chrome options
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.asurascans.com/manga/4569947261-genius-of-the-unique-lineage/")
 
-# Wait for the latest chapter link to appear
-chapter_locator = (By.CSS_SELECTOR, ".lastend .inepcx:last-child a")
-latest_chapter_link = WebDriverWait(driver, 10).until(EC.presence_of_element_located(chapter_locator))
+# Iterate over the book links
+for book_link in book_links:
+    # Load the book page
+    driver.get(book_link)
 
-# Get the href attribute of the latest chapter link
-latest_chapter = latest_chapter_link.get_attribute("href")
+    # Wait for the latest chapter link to appear
+    chapter_locator = (By.CSS_SELECTOR, ".lastend .inepcx:last-child a")
+    latest_chapter_link = WebDriverWait(driver, 10).until(EC.presence_of_element_located(chapter_locator))
 
-# Extract the chapter number from the link
-chapter_number = ""
-for i in range(len(latest_chapter) - 1, 0, -1):
-    if latest_chapter[i] == "-":
-        chapter_number = latest_chapter[i + 1: len(latest_chapter) - 1]
-        break
+    # Get the href attribute of the latest chapter link
+    latest_chapter = latest_chapter_link.get_attribute("href")
 
-# Print the latest chapter link
-print("Latest chapter:", latest_chapter)
-print("Chapter number:", chapter_number)
+    # Remove the trailing slash from the latest_chapter
+    latest_chapter = latest_chapter[:-1]
+
+    # Extract the chapter number from the link
+    chapter_number = latest_chapter.split("-")[-1]
+
+    # Print the latest chapter link and number
+    print("Book:", book_link)
+    print("Latest chapter:", latest_chapter)
+    print("Chapter number:", chapter_number)
+    print()
 
 # Quit the driver
 driver.quit()
